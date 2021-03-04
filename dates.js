54353fd18e0e952058091e3e64c4cfb4227988f7
Ocuriz <73173971+Ocuriz@users.noreply.github.com>
2021-03-04 20:04:46 +0100

    Update dates.js

--- dates.js
+++ dates.js
@@ -1,7 +1,7 @@
-[{"ville":"Rennes", "dep":35, "lieu":"L'étage", "date":"28/02/2014", "heure":"20h00" }, 
-{"ville":"Le Mans", "dep":72, "lieu":"L'oasis", "date":"19/02/2014", "heure":"20h00" }, 
-{"ville":"Niort", "dep":79, "lieu":"Le Loup Blanc", "date":"20/02/2014", "heure":"20h30" },
-{"ville":"Paris", "dep":75, "lieu":"Le Bataclan", "date":"04/03/2014", "heure":"19h30" }, 
-{"ville":"Reims", "dep":51, "lieu":"La Cartonnerie", "date":"13/03/2014", "heure":"20h00" },
-{"ville":"Lille", "dep":59, "lieu":"L'Aéronef", "date":"14/03/2014", "heure":"20h00" },
-{"ville":"Nantes", "dep":44, "lieu":"La Fabrique", "date":"19/03/2014", "heure":"20h30"}]
+[{"ville":"Rennes", "dep":35, "lieu":"L'étage", "date":"28/02/2021", "heure":"20h00" }, 
+{"ville":"Le Mans", "dep":72, "lieu":"L'oasis", "date":"19/02/2021", "heure":"20h00" }, 
+{"ville":"Niort", "dep":79, "lieu":"Le Loup Blanc", "date":"20/02/2021", "heure":"20h30" },
+{"ville":"Paris", "dep":75, "lieu":"Le Bataclan", "date":"04/03/2020", "heure":"19h30" }, 
+{"ville":"Reims", "dep":51, "lieu":"La Cartonnerie", "date":"13/03/2020", "heure":"20h00" },
+{"ville":"Lille", "dep":59, "lieu":"L'Aéronef", "date":"14/03/2020", "heure":"20h00" },
+{"ville":"Nantes", "dep":44, "lieu":"La Fabrique", "date":"19/03/2020", "heure":"20h30"}]
